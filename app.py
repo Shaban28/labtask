84cb0306b28d8e9fce17a7bf48265575bb2d358b
Shaban28 <124589309+Shaban28@users.noreply.github.com>
2025-03-01 03:01:43 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)  # Corrected version
 
 @app.route("/")
 def home():
-    return "hello,Shaban Qadeer Khan"
+    return "hello,Shaban"
 
 if __name__ == "__main__":
     app.run()
