5013b140c7747c044124e547c849814eb6a7fef9
Shaban28 <124589309+Shaban28@users.noreply.github.com>
2025-03-01 02:29:58 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)  # Corrected version
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello, my name is Mubeen and this is my app!"
+    return "Hello, World!"
 
-if _name_ == '_main_':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
